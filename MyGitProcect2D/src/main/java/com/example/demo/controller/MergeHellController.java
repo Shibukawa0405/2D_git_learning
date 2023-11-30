ea24f5bbc62bff5403d5374e0e3ea0b011576f72
S0544 <S0544@21S0544>
2023-11-30 09:48:16 +0900

    kadaiAメソッドを追加。10回ループするよ

---
+++ MyGitProcect2D/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+		kadaiB();
+	}
+
+	static void kadaiA() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "回目のループです");
+		}
+	}
+}
